87482b8bab68140e13b1573dcbb538a6144e7217
issac <issacfer@163.com>
2016-07-31 13:48:28 +0800

    版本更新

--- Controller/start.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: issac
- * Date: 2016/6/10
- * Time: 15:40
- */
--- Public/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: issac
- * Date: 2016/6/11
- * Time: 20:10
- */
-$urlHd="http://localhost/proI";
